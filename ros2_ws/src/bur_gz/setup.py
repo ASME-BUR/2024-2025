a75991ac49792f8c8f54439f8bfeb4dfaa7ca951
Prabhvir <theprabab@gmail.com>
2024-10-23 05:57:35 +0000

    Commit what I have
        pick 6662345 Create basic model for robot
        pick d78a12c Commit what I have
        pick 6147b9e Fix launch file
        pick 5b6b7b9 Make gazebo sim (still not working)

---
+++ ros2_ws/src/bur_gz/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import find_packages, setup
+from glob import glob
+
+# Iterate through all the files and subdirectories
+# to build the data files array
+def generate_data_files(share_path, dir):
+    data_files = []
+
+    for path, _, files in os.walk(dir):
+        list_entry = (share_path + path, [os.path.join(path, f) for f in files if not f.startswith('.')])
+        data_files.append(list_entry)
+
+    return data_files
+
+package_name = 'bur_gz'
+
+# Commented out directories will be relevant later
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch/', glob('launch/*')),
+        # ('share/' + package_name + '/config/', glob('config/*')),
+        ('share/' + package_name + '/urdf/', glob('urdf/*')),
+        # ('share/' + package_name + '/worlds/', glob('worlds/*')),
+        # ('share/' + package_name + '/meshes/visual', glob('meshes/visual/*')),
+        ('share/' + package_name + '/rviz/', glob('rviz/*')),
+    ] + generate_data_files('share/' + package_name + '/', 'models'),
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='theprabab@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
